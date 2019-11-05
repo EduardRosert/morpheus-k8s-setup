a0d3eade2dabcdf6481f55caa5b52ea2611813e6
Eduard Rosert <eduardrosert@Eduards-MacBook-Air.local>
2019-11-05 16:33:18 +0000

    worker setup

--- Makefile
+++ Makefile
@@ -1,10 +1,13 @@
 default: install-requirements-controller
 
 install-requirements-controller:
+	./disable-swap.sh
 	./disable-ipv6.sh
 	./install-requirements.sh
+	./setup-controller.sh
 
 install-requirements-worker:
 	./create-filesystem.sh
+	./disable-swap.sh
 	./disable-ipv6.sh
 	./install-requirements.sh
---
+++ setup-worker.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# fail fast and show errors
+set -eux
+
+# Morpheus default env for instance
+# IP_INTERNAL
+
+kubeadm_token=y5g1m2.uckqmkuewh5809iz
+hostname=$HOSTNAME
+discovery_timeout=30m0s
+kube_api_internal_address=$KUBE_API_ADDRESS
+
+kubeadm_conf=/root/kubeadm.conf
+cat > $kubeadm_conf <<EOF
+apiVersion: kubeadm.k8s.io/v1beta2
+kind: JoinConfiguration
+caCertPath: /etc/kubernetes/pki/ca.crt
+discovery:
+  bootstrapToken:
+    apiServerEndpoint: $kube_api_internal_address:443
+    token: $kubeadm_token
+    unsafeSkipCAVerification: true
+  timeout: $discovery_timeout
+  tlsBootstrapToken: $kubeadm_token
+nodeRegistration:
+  criSocket: /var/run/dockershim.sock
+  name: $hostname
+EOF
+
+# Initialise Kubernetes components.
+kubeadm join \
+	-v 3 \
+	--config $kubeadm_conf
